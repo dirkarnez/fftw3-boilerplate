06807ac5e84f66bc535f866de1b3928b4ce68b1b
dirkarnez <smalldirkalex@gmail.com>
2025-01-07 01:02:44 +0800

    - wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,60 @@
 #include <iostream>
 #include "fftw3.h"
 
+#define SAMPLE_FREQUENCY (44100)
+#define DURATION (0.5)
+// #define NUMBER_OF_SAMPLES ((SAMPLE_FREQUENCY) * (DURATION))
+
 #define _USE_MATH_DEFINES
 #include <cmath>
 
 int main(void)
 {
-  const int SAMPLE_FREQUENCY = 44100;
-  const int DURATION = 0.5;
-  const int number_of_samples = SAMPLE_FREQUENCY * DURATION;
+//   fftw_complex in[NUMBER_OF_SAMPLES];
+//   fftw_complex out[NUMBER_OF_SAMPLES]; //, in2[number_of_samples]; /* double [2] */
+//   fftw_plan p, q;
+//   int i;
+  const int NUMBER_OF_SAMPLES = (SAMPLE_FREQUENCY) * (DURATION);
 
-  fftw_complex in[number_of_samples], out[number_of_samples]; //, in2[number_of_samples]; /* double [2] */
-  fftw_plan p, q;
-  int i;
+  double *in = (double*) fftw_malloc(sizeof(double) * 2*NUMBER_OF_SAMPLES);
+  fftw_complex *fft_result= (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * (NUMBER_OF_SAMPLES + 2));
 
-  /* prepare a cosine wave */
-  for (i = 0; i < number_of_samples; i++)
-  {
-    in[i][0] = 0.7 * cos(2 * M_PI * 5000 * (i / SAMPLE_FREQUENCY));
-    in[i][1] = 0;
+  double time_intervals[NUMBER_OF_SAMPLES];
+  double cos_wave[NUMBER_OF_SAMPLES];
+  // fftwf_complex* cos_wave = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * NUMBER_OF_SAMPLES);
+
+  // Generate cosine wave
+  for (int i = 0; i < NUMBER_OF_SAMPLES; i++) {
+    time_intervals[i] = (double)i / SAMPLE_FREQUENCY;
+    cos_wave[i]= 0.7 * cos(2 * M_PI * 5000 * time_intervals[i]);
   }
+
+  fftw_plan plan = fftw_plan_dft_r2c_1d(2 * NUMBER_OF_SAMPLES, cos_wave, fft_result, FFTW_PATIENT);
+
+
+  // /* prepare a cosine wave */
+  // for (i = 0; i < NUMBER_OF_SAMPLES; i++)
+  // {
+  //   in[i][0] = 0.7 * cos(2 * M_PI * 5000 * (i / SAMPLE_FREQUENCY));
+  //   in[i][1] = 0;
+  // }
 
   /* forward Fourier transform, save the result in 'out' */
-  p = fftw_plan_dft_1d(number_of_samples, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-  fftw_execute(p);
-  
-  for (i = 0; i < number_of_samples; i++)
-  {
-    printf("freq: %3d %+9.5f %+9.5f I\n", i, out[i][0], out[i][1]);
-  }
+  // p = fftw_plan_dft_1d(NUMBER_OF_SAMPLES, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+   fftw_execute(plan);
 
-  fftw_destroy_plan(p);
+  int half_samples = NUMBER_OF_SAMPLES / 2;
+  double* frequencies = (double*)malloc(half_samples * sizeof(double));
+  double* magnitudes = (double*)malloc(half_samples * sizeof(double));
+
+    for (int i = 0; i < half_samples; i++) {
+        frequencies[i] = (double)SAMPLE_FREQUENCY * i / NUMBER_OF_SAMPLES;
+        magnitudes[i] = sqrt(fft_result[i][0] * fft_result[i][0] + fft_result[i][1] * fft_result[i][1]) / half_samples;
+        std::cout << frequencies[i] << ":" << magnitudes[i] << std::endl;
+        
+    }
+
+  fftw_destroy_plan(plan);
 
   // /* backward Fourier transform, save the result in 'in2' */
   // printf("\nInverse transform:\n");
